bac848665f44fffd62ee02c4172d96355275bfd9
GuillermoBaldan <guillermobaldan@gmail.com>
2022-12-01 13:11:30 +0100

    js element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+let flag = false;
 $(function(){
 	$('[data-toggle="tooltip"]').tooltip()
 })
+
+
+console.log(document.getElementsByTagName("marquee")[0])
+document.getElementsByTagName("marquee")[0].addEventListener("mouseover", function(){
+    document.getElementsByTagName("marquee")[0].innerHTML = "HAZ CLICK AQUI Y RECIBELAS EN TU DOMICILIO"
+    setTimeout(function(){
+    document.getElementsByTagName("marquee")[0].innerHTML = "¡OFERTA: Media docena de chinchonadas a 1,99€!"
+    }, 3000)
+
+})
+
+/* document.addEventListener("mouseover", function(){
+    if( flag === true){
+    document.getElementsByTagName("marquee")[0].innerHTML = "¡OFERTA: Media docena de chinchonadas a 1,99€!"
+    }
+}) */
+
